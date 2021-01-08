e8acd9529248bcedd418a14941ed04dc5303c58d
Nguyen Thi Cam Tien <tientien.6899@gmail.com>
2021-01-09 02:46:38 +0700

    Update 9/1/2021

---
+++ app/src/main/java/com/example/coffeebakeryadmin/DetailCustomerActivity.java
@@ -0,0 +1,14 @@
+package com.example.coffeebakeryadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailCustomerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_customer);
+    }
+}
