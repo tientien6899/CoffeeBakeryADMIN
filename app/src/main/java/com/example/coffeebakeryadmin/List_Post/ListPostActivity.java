4b9d8f2e6cd649c36c4b3a461779fef9c1ffd341
Nguyen Thi Cam Tien <tientien.6899@gmail.com>
2021-01-23 23:33:32 +0700

    up tiep

--- app/src/main/java/com/example/coffeebakeryadmin/List_Post/ListPostActivity.java
+++ app/src/main/java/com/example/coffeebakeryadmin/List_Post/ListPostActivity.java
@@ -1,16 +1,48 @@
 package com.example.coffeebakeryadmin.List_Post;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.example.coffeebakeryadmin.AdminActivity;
 import com.example.coffeebakeryadmin.R;
 
 public class ListPostActivity extends AppCompatActivity {
+
+    ImageView img_backdsbv, img_adddsbv;
+    RecyclerView listpost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_post);
+
+        AnhXa();
+
+        img_backdsbv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListPostActivity.this, AdminActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        img_adddsbv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListPostActivity.this, AddPostActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void AnhXa() {
+        img_backdsbv = findViewById(R.id.img_BackDSBV);
+        img_adddsbv = findViewById(R.id.img_AddDSBV);
+        listpost = findViewById(R.id.list_post);
     }
 }
