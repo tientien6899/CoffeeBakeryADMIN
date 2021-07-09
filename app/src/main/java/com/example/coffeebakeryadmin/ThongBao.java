fdb8f2c92b29bd7c83893ea5e3503fa6f5b677f3
hmquy <mauquy123@gmail.com>
2021-07-09 19:42:20 +0700

    update

---
+++ app/src/main/java/com/example/coffeebakeryadmin/ThongBao.java
@@ -0,0 +1,30 @@
+package com.example.coffeebakeryadmin;
+
+public class ThongBao {
+    String tieude;
+    String noidung;
+
+    public ThongBao() {
+    }
+
+    public ThongBao(String tieude, String noidung) {
+        this.tieude = tieude;
+        this.noidung = noidung;
+    }
+
+    public String getTieude() {
+        return tieude;
+    }
+
+    public void setTieude(String tieude) {
+        this.tieude = tieude;
+    }
+
+    public String getNoidung() {
+        return noidung;
+    }
+
+    public void setNoidung(String noidung) {
+        this.noidung = noidung;
+    }
+}
